cf8e3632fa75033543a358825fc270c04ae2bb59
capm4 <kruglov.aleksandr@gmail.com>
2017-02-11 10:17:06 +0200

    Great link

--- initDB.sql
+++ initDB.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS companies (
-  id      INT AUTO_INCREMENT NOT NULL  PRIMARY KEY ,
+  id      INT NOT NULL AUTO_INCREMENT  PRIMARY KEY ,
   name    VARCHAR(100) NOT NULL ,
   address VARCHAR(100) NOT NULL ,
   country VARCHAR(100) NOT NULL ,
@@ -10,14 +10,12 @@ CREATE TABLE IF NOT EXISTS companies (
 ENGINE InnoDB;
 
 CREATE TABLE IF NOT EXISTS developers (
-  id INT AUTO_INCREMENT NOT NULL  PRIMARY KEY ,
+  id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY  ,
   name VARCHAR(100) NOT NULL ,
   address VARCHAR(100) NOT NULL ,
   country VARCHAR(100) NOT NULL ,
   city  VARCHAR(100) NOT NULL ,
   join_date date,
-  skills_id INT,
-  FOREIGN KEY (skills_id) REFERENCES skills(developer_id),
 
   INDEX (name)
 )
@@ -26,15 +24,26 @@ ENGINE InnoDB;
 CREATE TABLE IF NOT EXISTS skills(
   developer_id INT,
   JAVA VARCHAR (1),
-  "C++" VARCHAR (1),
-  "C#" VARCHAR (1),
+  CPlusPlus VARCHAR (1),
+  CSharp VARCHAR (1),
   JavaScript VARCHAR(1),
   PHP VARCHAR(1),
   PYTHON VARCHAR(1),
   RUBY VARCHAR(1),
-  SCALA VARCHAR(1),
-  FOREIGN KEY (developer_id) REFERENCES developers(skills_id)
+  SCALA VARCHAR(1)
 )
 ENGINE InnoDB;
+
+CREATE TABLE IF NOT EXISTS Dev_Skills(
+  developer_id INT NOT NULL,
+  skills_id    INT NOT NULL,
+
+  FOREIGN KEY (developer_id) REFERENCES developers(id),
+  FOREIGN KEY (skills_id) REFERENCES skills(developer_id),
+
+  UNIQUE (developer_id, skills_id)
+)
+ENGINE InnoBD;
+
 
 
